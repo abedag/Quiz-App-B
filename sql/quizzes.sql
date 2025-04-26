de848beb8352abf942c0da5dfa14915dab4a1233
abedag <abedalrahman7117@gmail.com>
2025-04-27 02:53:26 +0300

    Add <quizzes.sql> Adding quizzes (defult ones)

--- sql/quizzes.sql
+++ sql/quizzes.sql
@@ -7,4 +7,12 @@ CREATE TABLE quizzes (
 	FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE SET NULL
 );
 
+INSERT INTO quizzes (title, section_id, created_by) VALUES 
+('Frontend Basics Quiz', (SELECT id FROM sections WHERE name = 'frontend'), 1),
+('Backend Basics Quiz', (SELECT id FROM sections WHERE name = 'backend'), 1),
+('C/C++ Basics Quiz', (SELECT id FROM sections WHERE name = 'c_cpp'), 1),
+('Algorithms Basics Quiz', (SELECT id FROM sections WHERE name = 'algorithms'), 1),
+('DevOps Basics Quiz', (SELECT id FROM sections WHERE name = 'devops'), 1),
+('Software Engineering Basics Quiz', (SELECT id FROM sections WHERE name = 'software_eng'), 1);
+
 SELECT * FROM quizzes;
