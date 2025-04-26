7a48235aab4945e1d6dfa9e5b0951ebee2012fe1
abedag <abedalrahman7117@gmail.com>
2025-04-27 02:42:23 +0300

    Add <delete_question.php> Adding execute with handling errors

--- php/delete_question.php
+++ php/delete_question.php
@@ -13,7 +13,15 @@ try {
 
   $query -> bindParam(':question_id', $question_id, PDO::PARAM_INT);
 
-
+  if ($query -> execute()) {
+    if ($query -> rowCount() > 0) {
+      echo json_encode(["message" => "Question deleted successfully"]);
+    } else {
+      echo json_encode(["error" => "Question not found"]);
+    }
+  } else {
+    echo json_encode(["error" => "Failed to delete question"]);
+  }
 
 } catch(PDOException $e) {
   echo json_encode(["error" => "Question deletion error:" . $e -> getMessage()]);
