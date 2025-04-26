3069715741942a2492676885cda13dda8a426a9e
abedag <abedalrahman7117@gmail.com>
2025-04-26 23:58:51 +0300

    Add <users.sql> Creating users table

--- sql/users.sql
+++ sql/users.sql
@@ -1,2 +1,13 @@
 CREATE DATABASE webAppDB;
 USE webAppDB;
+
+CREATE TABLE users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(55) UNIQUE NOT NULL,
+  username VARCHAR(20) UNIQUE NOT NULL,
+  password VARCHAR(55) NOT NULL,
+  scores_json TEXT,
+  high_score INT DEFAULT 0,
+  role ENUM('user', 'admin') DEFAULT 'user'
+);
+
