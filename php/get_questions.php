7ae2434533323c75fe22311c132c0dd639149622
abedag <abedalrahman7117@gmail.com>
2025-04-27 02:46:06 +0300

    Add <get_questions.php> Adding getting questions' options with handling errors

--- php/get_questions.php
+++ php/get_questions.php
@@ -18,7 +18,25 @@ $questions = $query -> fetchAll(PDO::FETCH_ASSOC);
 
 $result = [];
 
+foreach ($questions as $question) {
+  $option_query = $conn->prepare("
+      SELECT id AS option_id, option_text, is_correct
+      FROM q_options
+      WHERE question_id = :question_id
+  ");
+  $option_query->bindParam(':question_id', $question['question_id'], PDO::PARAM_INT);
+  $option_query->execute();
+  $options = $option_query->fetchAll(PDO::FETCH_ASSOC);
 
+  $result[] = [
+      "question_id" => $question['question_id'],
+      "question_text" => $question['question_text'],
+      "quiz_title" => $question['quiz_title'],
+      "options" => $options
+  ];
+}
+
+echo json_encode($result);
 
 } catch (PDOException $e) {
 echo json_encode(["error" => "Questions call error:" . $e->getMessage()]);
