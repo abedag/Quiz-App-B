6e48efad65d80737d388c96ebf1c56e99e7dacf9
abedag <abedalrahman7117@gmail.com>
2025-04-27 01:04:43 +0300

    Add <get_quizzes.php> Adding inputs to get all quizzes (with query)

---
+++ php/get_quizzes.php
@@ -0,0 +1,24 @@
+<?php
+include 'connection.php';
+
+try {
+  $query = $conn -> prepare("SELECT name FROM sections");
+  
+  $title = $_GET['title'];
+  $section_id = $_GET['section_id'];
+  $created_by = $_GET['created_by'];
+
+  $query -> bindParam(':title', $title);
+  $query -> bindParam(':section_id', $section_id);
+  $query -> bindParam(':created_by', $created_by);
+
+  $quizzes = $query->fetchAll(PDO::FETCH_ASSOC);
+
+  echo json_encode($quizzes);
+
+
+} catch(PDOException $e) {
+  echo json_encode(["error" => "Quizzes call error:" . $e -> getMessage()]);
+}
+
+?>
