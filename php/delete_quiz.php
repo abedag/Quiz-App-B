812d3644bb0b300a5a1e69ec37f5a9a831704026
abedag <abedalrahman7117@gmail.com>
2025-04-27 01:02:22 +0300

    Add <delete_quiz.php> Adding execute with handling errors

--- php/delete_quiz.php
+++ php/delete_quiz.php
@@ -13,7 +13,15 @@ try {
 
   $query->bindParam(':quiz_id', $quiz_id, PDO::PARAM_INT);
 
-
+  if ($query->execute()) {
+    if ($query->rowCount() > 0) {
+      echo json_encode(["message" => "Quiz deleted successfully"]);
+    } else {
+      echo json_encode(["error" => "Quiz not found"]);
+    }
+  } else {
+    echo json_encode(["error" => "Failed to delete quiz"]);
+  }
 
 } catch(PDOException $e) {
   echo json_encode(["error" => "Quiz deletion error:" . $e -> getMessage()]);
