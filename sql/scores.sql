cbe17f7bcd02fc80cdabe69a3ba77aad29ecd722
abedag <abedalrahman7117@gmail.com>
2025-04-27 00:05:24 +0300

    Add <scores.sql> Creating scores table

---
+++ sql/scores.sql
@@ -0,0 +1,8 @@
+CREATE TABLE scores (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT NOT NULL,
+  quiz_id INT NOT NULL,
+  score INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (quiz_id) REFERENCES quizzes(id) ON DELETE CASCADE
+);
