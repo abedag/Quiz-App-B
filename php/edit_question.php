7de686e149643ceb8b52132d849e2ecbdbd1ae20
abedag <abedalrahman7117@gmail.com>
2025-04-27 02:19:21 +0300

    Add <edit_question.php> Adding update question text option

--- php/edit_question.php
+++ php/edit_question.php
@@ -9,6 +9,13 @@ try {
 
   $question_id = $_POST['question_id'];
   
+  if (!empty($_POST['question_text'])) {
+    $query = $conn->prepare("UPDATE questions SET question_text = :question_text WHERE id = :question_id");
+    $query->bindParam(':question_text', $_POST['question_text']);
+    $query->bindParam(':question_id', $question_id, PDO::PARAM_INT);
+    $query->execute();
+  }
+
 } catch (PDOException $e) {
   echo json_encode(["error" => $e->getMessage()]);
 }
